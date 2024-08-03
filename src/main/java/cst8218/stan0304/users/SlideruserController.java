8358ae40ebe5e1e8edb5878d4a361fe688986e40
capmontulet <108439458+capmontulet@users.noreply.github.com>
2024-08-03 13:07:59 -0400

    added slideruser table and entity

---
+++ src/main/java/cst8218/stan0304/users/SlideruserController.java
@@ -0,0 +1,239 @@
+package cst8218.stan0304.users;
+
+import cst8218.stan0304.users.util.JsfUtil;
+import cst8218.stan0304.users.util.PaginationHelper;
+
+import java.io.Serializable;
+import java.util.ResourceBundle;
+import jakarta.annotation.Resource;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.component.UIComponent;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.convert.Converter;
+import jakarta.faces.convert.FacesConverter;
+import jakarta.faces.model.DataModel;
+import jakarta.faces.model.ListDataModel;
+import jakarta.faces.model.SelectItem;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceUnit;
+import jakarta.transaction.UserTransaction;
+
+@Named("slideruserController")
+@SessionScoped
+public class SlideruserController implements Serializable {
+
+    @Resource
+    private UserTransaction utx = null;
+    @PersistenceUnit(unitName = "slider_persistence_unit")
+    private EntityManagerFactory emf = null;
+
+    private Slideruser current;
+    private DataModel items = null;
+    private SlideruserJpaController jpaController = null;
+    private PaginationHelper pagination;
+    private int selectedItemIndex;
+
+    public SlideruserController() {
+    }
+
+    public Slideruser getSelected() {
+        if (current == null) {
+            current = new Slideruser();
+            selectedItemIndex = -1;
+        }
+        return current;
+    }
+
+    private SlideruserJpaController getJpaController() {
+        if (jpaController == null) {
+            jpaController = new SlideruserJpaController(utx, emf);
+        }
+        return jpaController;
+    }
+
+    public PaginationHelper getPagination() {
+        if (pagination == null) {
+            pagination = new PaginationHelper(10) {
+
+                @Override
+                public int getItemsCount() {
+                    return getJpaController().getSlideruserCount();
+                }
+
+                @Override
+                public DataModel createPageDataModel() {
+                    return new ListDataModel(getJpaController().findSlideruserEntities(getPageSize(), getPageFirstItem()));
+                }
+            };
+        }
+        return pagination;
+    }
+
+    public String prepareList() {
+        recreateModel();
+        return "List";
+    }
+
+    public String prepareView() {
+        current = (Slideruser) getItems().getRowData();
+        selectedItemIndex = pagination.getPageFirstItem() + getItems().getRowIndex();
+        return "View";
+    }
+
+    public String prepareCreate() {
+        current = new Slideruser();
+        selectedItemIndex = -1;
+        return "Create";
+    }
+
+    public String create() {
+        try {
+            getJpaController().create(current);
+            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("SlideruserCreated"));
+            return prepareCreate();
+        } catch (Exception e) {
+            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
+            return null;
+        }
+    }
+
+    public String prepareEdit() {
+        current = (Slideruser) getItems().getRowData();
+        selectedItemIndex = pagination.getPageFirstItem() + getItems().getRowIndex();
+        return "Edit";
+    }
+
+    public String update() {
+        try {
+            getJpaController().edit(current);
+            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("SlideruserUpdated"));
+            return "View";
+        } catch (Exception e) {
+            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
+            return null;
+        }
+    }
+
+    public String destroy() {
+        current = (Slideruser) getItems().getRowData();
+        selectedItemIndex = pagination.getPageFirstItem() + getItems().getRowIndex();
+        performDestroy();
+        recreatePagination();
+        recreateModel();
+        return "List";
+    }
+
+    public String destroyAndView() {
+        performDestroy();
+        recreateModel();
+        updateCurrentItem();
+        if (selectedItemIndex >= 0) {
+            return "View";
+        } else {
+            // all items were removed - go back to list
+            recreateModel();
+            return "List";
+        }
+    }
+
+    private void performDestroy() {
+        try {
+            getJpaController().destroy(current.getId());
+            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("SlideruserDeleted"));
+        } catch (Exception e) {
+            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
+        }
+    }
+
+    private void updateCurrentItem() {
+        int count = getJpaController().getSlideruserCount();
+        if (selectedItemIndex >= count) {
+            // selected index cannot be bigger than number of items:
+            selectedItemIndex = count - 1;
+            // go to previous page if last page disappeared:
+            if (pagination.getPageFirstItem() >= count) {
+                pagination.previousPage();
+            }
+        }
+        if (selectedItemIndex >= 0) {
+            current = getJpaController().findSlideruserEntities(1, selectedItemIndex).get(0);
+        }
+    }
+
+    public DataModel getItems() {
+        if (items == null) {
+            items = getPagination().createPageDataModel();
+        }
+        return items;
+    }
+
+    private void recreateModel() {
+        items = null;
+    }
+
+    private void recreatePagination() {
+        pagination = null;
+    }
+
+    public String next() {
+        getPagination().nextPage();
+        recreateModel();
+        return "List";
+    }
+
+    public String previous() {
+        getPagination().previousPage();
+        recreateModel();
+        return "List";
+    }
+
+    public SelectItem[] getItemsAvailableSelectMany() {
+        return JsfUtil.getSelectItems(getJpaController().findSlideruserEntities(), false);
+    }
+
+    public SelectItem[] getItemsAvailableSelectOne() {
+        return JsfUtil.getSelectItems(getJpaController().findSlideruserEntities(), true);
+    }
+
+    @FacesConverter(forClass = Slideruser.class)
+    public static class SlideruserControllerConverter implements Converter {
+
+        @Override
+        public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
+            if (value == null || value.length() == 0) {
+                return null;
+            }
+            SlideruserController controller = (SlideruserController) facesContext.getApplication().getELResolver().
+                    getValue(facesContext.getELContext(), null, "slideruserController");
+            return controller.getJpaController().findSlideruser(getKey(value));
+        }
+
+        java.lang.Double getKey(String value) {
+            java.lang.Double key;
+            key = Double.valueOf(value);
+            return key;
+        }
+
+        String getStringKey(java.lang.Double value) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(value);
+            return sb.toString();
+        }
+
+        @Override
+        public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
+            if (object == null) {
+                return null;
+            }
+            if (object instanceof Slideruser) {
+                Slideruser o = (Slideruser) object;
+                return getStringKey(o.getId());
+            } else {
+                throw new IllegalArgumentException("object " + object + " is of type " + object.getClass().getName() + "; expected type: " + Slideruser.class.getName());
+            }
+        }
+
+    }
+
+}
---
+++ src/main/java/cst8218/stan0304/users/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package cst8218.stan0304.users.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
